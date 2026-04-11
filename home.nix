7659a49ac8d9acdf55f3a0e1e3d71386daed7bb2
ChunHou20c <chunhouthatbornin20c@gmail.com>
2026-04-11 20:26:07 +0800

    update home module

--- home.nix
+++ home.nix
@@ -10,7 +10,27 @@
     pkgs.git
     pkgs.curl
     pkgs.htop
+
+    pkgs.cloudflared
   ];
+
+  systemd.user.services.cloudflared = {
+    Unit = {
+      Description = "Cloudflare Tunnel";
+      After = [ "network.target" ];
+    };
+
+    Service = {
+      ExecStart = "${pkgs.cloudflared}/bin/cloudflared tunnel run --token YOUR_TOKEN";
+      Restart = "always";
+      RestartSec = 5;
+      Environment = "TUNNEL_TOKEN=your_token_here";
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
 
   programs.home-manager.enable = true;
 }
